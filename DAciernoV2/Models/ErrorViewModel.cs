d9950147c10b38f17915411be2b4fed3f7e82dda
Sam Underwood <stu17@pitt.edu>
2018-02-06 16:44:44 -0500

    Beginning + Background

---
+++ DAciernoV2/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace DAciernoV2.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
